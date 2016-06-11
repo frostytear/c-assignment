6c8d23bf88dafe15380f970a4fed1cddf8c3cc3a
frostytear <frostytear@users.noreply.github.com>
2016-06-11 22:18:57 +0800

    Add files via upload

---
+++ 3053.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int n, i, j;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		for(j=1; j<=n-i; j++){
+			printf(" ");
+		}
+		for(j=1; j<=i-1; j++){
+			printf("* ");
+		}
+		printf("*\n");
+	}
+	return 0;
+}
